5d1ce1ec59d8419697f2f8852c71a5cfe799a298
oahlers <oliv2527@stud.kea.dk>
2025-09-29 10:35:24 +0200

    setup#5 - pagecontroller added

---
+++ src/main/java/com/example/jor_opgave1_kinoxp/controller/PageController.java
@@ -0,0 +1,43 @@
+package com.example.jor_opgave1_kinoxp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    @GetMapping("/movies")
+    public String movies() {
+        return "movies.html";
+    }
+
+    @GetMapping("/shows")
+    public String shows() {
+        return "shows.html";
+    }
+
+    @GetMapping("/bookings")
+    public String bookings() {
+        return "bookings.html";
+    }
+
+    @GetMapping("/staff")
+    public String staff() {
+        return "staff.html";
+    }
+
+    @GetMapping("/roster")
+    public String roster() {
+        return "roster.html";
+    }
+
+    @GetMapping("/sweets")
+    public String sweets() {
+        return "sweets.html";
+    }
+}
