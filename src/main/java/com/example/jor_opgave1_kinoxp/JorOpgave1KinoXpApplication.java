9d4791bdf75bfd3dcf3f665c86a5992c1428394b
oahlers <oliv2527@stud.kea.dk>
2025-09-29 09:26:01 +0200

    .

---
+++ src/main/java/com/example/jor_opgave1_kinoxp/JorOpgave1KinoXpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jor_opgave1_kinoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorOpgave1KinoXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JorOpgave1KinoXpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jor_opgave1_kinoxp/JorOpgave1KinoXpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jor_opgave1_kinoxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JorOpgave1KinoXpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
